40a42b2dd4a06cd9dc7b03ed12655a8bec1984cd
Vikas Khatri <vikas.khatri@ericsson.com>
2018-10-15 18:32:47 +0530

    fff

--- SeleniumFramework/src/test/java/Browsertest.java
+++ SeleniumFramework/src/test/java/Browsertest.java
@@ -7,12 +7,12 @@ public class Browsertest {
    public static void main(String[] args) {
 	   System.setProperty("webdriver.gecko.driver", "C:\\Tools\\Eclipse\\SeleniumFramework\\Driver\\GeckoDriver\\geckodriver-v0.23.0-win64\\geckodriver.exe");
 	 WebDriver Driver = new FirefoxDriver();
-	 Driver.get("http://localhost:9090/jenkins/login?from=%2Fjenkins/");
+	 Driver.get("http://10.184.20.118:8080/login?from=%2F/");
    
    
    
-	  Driver.findElement(By.id("j_username")).sendKeys("vik");
-	  Driver.findElement(By.name("j_password")).sendKeys("Password@123");
+	  Driver.findElement(By.id("j_username")).sendKeys("neerajbhartiya");
+	  Driver.findElement(By.name("j_password")).sendKeys("password@123");
 
 	  Driver.findElement(By.name("Submit")).click();
 }
